7761c5b166e7213467eae22f5194df2280efef8c
Akola Mbey Denis <akoladenis97@gmail.com>
2019-08-24 22:24:20 +0000

    initial commit

--- BANKING_SYSTEM_DESIGN_AND_IMPLEMENETATION/src/savingsAcccount.cpp
+++
@@ -1,8 +0,0 @@
-#include "savingsAcccount.h"
-
-savingsAcccount::savingsAcccount(string fname,string lname,long accountN,double amount):bankAccount(fname,lname,accountN,amount)
-{
-
-}
-double savingsAcccount::depositMoney()
-
